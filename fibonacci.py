0edcd5949cce583d714f9ed226851211c121189a
H. Mondal <167565422+hrimondal@users.noreply.github.com>
2024-06-28 00:06:05 +0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -4,6 +4,10 @@ def fibonacci(n):
           a,b = b, a + b
           
     return a 
-fibonacci_2019 = fibonacci(2019)
-print(fibonacci_2019)
-
+# in below code you have to add the serial number of the term
+# suppose you want to find out the n-th term.
+# fibonacci_n = fibonacci(n)
+fibonacci_20 = fibonacci(20)
+# print(fibonacci_n)
+print(fibonacci_20)
+# this code will primarily give output of the 20th term
