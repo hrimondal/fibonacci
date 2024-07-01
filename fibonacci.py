ad156deb525524065df52cf30c7e8f161f39c549
H. Mondal <167565422+hrimondal@users.noreply.github.com>
2024-07-01 20:00:38 +0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,4 @@
-def fib(term):
-    if term <= 1:
-        return term
-    else:
-        return fib(term - 1) + fib(term - 2)
 
-
-# Change this value to adjust the number of terms in the sequence.
-number_of_terms = int(input())
-for i in range(number_of_terms):
-    print(fib(i))
-
-
-# Or,
 def fibonacci(n):
   """
   This function calculates the nth term of the Fibonacci sequence.
