5bd8c214fe24dc34a32051a72207308f36302e00
H. Mondal <167565422+hrimondal@users.noreply.github.com>
2024-07-01 19:59:27 +0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -9,3 +9,20 @@ def fib(term):
 number_of_terms = int(input())
 for i in range(number_of_terms):
     print(fib(i))
+
+
+# Or,
+def fibonacci(n):
+  """
+  This function calculates the nth term of the Fibonacci sequence.
+  """
+  if n <= 1:
+    return n
+  else:
+    return fibonacci(n-1) + fibonacci(n-2)
+
+# Get user input for the term
+term = int(input("Enter the term you want to find: "))
+
+# Calculate and print the nth Fibonacci term
+print(fibonacci(term))
